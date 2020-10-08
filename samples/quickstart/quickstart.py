d90d7160743a3afea8d260a99659514c9a576411
Dan O'Meara <danom@google.com>
2020-10-08 19:58:40 +0000

    chore: blacken

--- samples/quickstart/quickstart.py
+++ samples/quickstart/quickstart.py
@@ -30,33 +30,36 @@ def run_quickstart():
     video_client = videointelligence.VideoIntelligenceServiceClient()
     features = [videointelligence.enums.Feature.LABEL_DETECTION]
     operation = video_client.annotate_video(
-        'gs://cloud-samples-data/video/cat.mp4', features=features)
-    print('\nProcessing video for label annotations:')
+        "gs://cloud-samples-data/video/cat.mp4", features=features
+    )
+    print("\nProcessing video for label annotations:")
 
     result = operation.result(timeout=120)
-    print('\nFinished processing.')
+    print("\nFinished processing.")
 
     # first result is retrieved because a single video was processed
     segment_labels = result.annotation_results[0].segment_label_annotations
     for i, segment_label in enumerate(segment_labels):
-        print(f'Video label description: {segment_label.entity.description}'
-            segment_label.entity.description))
+        print(f"Video label description: {segment_label.entity.description}")
         for category_entity in segment_label.category_entities:
-            print(f'\tLabel category description: {category_entity.description}'
-                category_entity.description))
+            print(f"\tLabel category description: {category_entity.description}")
 
         for i, segment in enumerate(segment_label.segments):
-            start_time = (segment.segment.start_time_offset.seconds +
-                          segment.segment.start_time_offset.nanos / 1e9)
-            end_time = (segment.segment.end_time_offset.seconds +
-                        segment.segment.end_time_offset.nanos / 1e9)
-            positions = f'{start_time}s to {end_time}s'
+            start_time = (
+                segment.segment.start_time_offset.seconds
+                + segment.segment.start_time_offset.nanos / 1e9
+            )
+            end_time = (
+                segment.segment.end_time_offset.seconds
+                + segment.segment.end_time_offset.nanos / 1e9
+            )
+            positions = f"{start_time}s to {end_time}s"
             confidence = segment.confidence
-            print(f'\tSegment {i}: {positions}')
-            print(f'\tConfidence: {confidence}')
-        print('\n')
+            print(f"\tSegment {i}: {positions}")
+            print(f"\tConfidence: {confidence}")
+        print("\n")
     # [END video_quickstart]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_quickstart()
